fd2e4c4282b96bbc7408beb8138e81df5c675c2a
Gill <69636661+gill-dev@users.noreply.github.com>
2024-08-16 09:30:53 +0000

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,15 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-      domains: ['localhost'], // Add any other domains you might use
+      domains: ['localhost'], 
     },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/{vr}' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/{vr}' : '',
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+    basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  
   }
 
 export default nextConfig;
